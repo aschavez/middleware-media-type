6732ac2bb477c6eb54d7790ad47b8a56a573e921
Andres Chavez Ulfe <achavez@Andres-Chavez.local>
2016-11-29 15:41:44 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os.path
+from setuptools import setup, find_packages
+
+package_dir = os.path.abspath(os.path.dirname(__file__))
+version_file = os.path.join(package_dir, "version")
+with open(version_file) as version_file_handle:
+    version = version_file_handle.read()
+
+setup(
+    name = "middleware-media-type",
+    version = version,
+    description = "Falcon Media type middlware",
+    package_dir = {"":"src"},
+    packages = find_packages("src"),
+    install_requires=[
+        "falcon",
+        "dicttoxml",
+        "git+ssh://git@git.rcp.pe:4488/devteam/falcon-exceptions.git#egg=falcon_exceptions"
+    ],
+    author = 'DevTeam RCP',
+    author_email = 'devteam@rcp.pe',
+    url = 'https://git.rcp.pe/devteam/middleware-media-type',
+    keywords = ['falcon', 'media', 'type']
+)
