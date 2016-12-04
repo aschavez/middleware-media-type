b71025d17fcb85b32839a8ae3e979711b5eaa089
Andrés Chávez <achavez@rcp.pe>
2016-12-04 12:37:46 -0500

    Fix http status code and response

--- src/middleware_media_type/middleware.py
+++ src/middleware_media_type/middleware.py
@@ -9,14 +9,14 @@ class RequireJSON(object):
 
     def process_request(self, req, resp):
         if not req.client_accepts_json and not req.client_accepts_xml:
-            raise HTTPException(406,
+            raise HTTPException(415,
                 "This API only supports responses encoded as JSON or XML",
                 "This API only supports responses encoded as JSON or XML")
         if req.method in ('POST', 'PUT'):
             if 'application/json' not in req.content_type:
-                raise falcon.HTTPUnsupportedMediaType(
-                    'This API only supports requests encoded as JSON.',
-                    href='http://docs.examples.com/api/json')
+                raise HTTPException(415,
+                    "This API only supports responses encoded as JSON or XML",
+                    "This API only supports responses encoded as JSON or XML")
 
 
 class ParseMediaType(object):
